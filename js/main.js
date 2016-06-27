954382406f7ebf9c5ee37846535a1ce07b97da04
Bogdan <bchojecki@onet.pl>
2016-06-27 11:30:19 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $( document ).ready(function() {
 $('.search').on('click', function() {
 $('#search').toggleClass('aa');
 });
-$(".burgernav").click(function () {
-$('#burgernavy').fadeToggle(300);
+$('.burgernav').on('click', function() {
+$('#burgernavy').toggleClass('aa');
 });   
 });
+
